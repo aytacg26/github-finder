06ba8d7929443db127f9e3f91564ebebbffd3b5f
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-05 22:56:58 +0200

    Spinner Added to Users Component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-const Users = ({ users }) => {
-  const userStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)',
-    gridGap: '1rem',
-  };
-
-  return (
+const Users = ({ users, loading }) => {
+  const usersView = loading ? (
+    <Spinner />
+  ) : (
     <div style={userStyle}>
       {users.map((user) => (
         <UserItem key={user.id} user={user} />
       ))}
     </div>
   );
+
+  return usersView;
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem',
 };
 
 export default Users;
