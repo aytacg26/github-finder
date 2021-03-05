8c19ca2d0624b1917d9d2a5ff35ab07c7b71c4e1
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-05 22:19:29 +0200

    state added to App and api call added to it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import { Component } from 'react';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    this.getUsers();
+  }
+
+  getUsers = async () => {
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+  };
+
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <UserItem />
+        <div className='container'>
+          <Users users={this.state.users} />
+        </div>
       </div>
     );
   }
