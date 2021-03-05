8397e0efe7c505ef742866d435dcecbdc8fcec2a
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-05 21:34:47 +0200

    UserItem Component Added to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <Navbar title='Github Finder' icon='fab fa-github' />
+        <Navbar />
+        <UserItem />
       </div>
     );
   }
