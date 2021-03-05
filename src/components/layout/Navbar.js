7a3c971433ebd8d9d06e7e6b6888100ae451c17f
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-05 21:35:16 +0200

    default props and proptypes added to Nav component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 //Will be converted to functional based component
 class Navbar extends Component {
+  static defaultProps = {
+    title: 'Github Finder',
+    icon: 'fab fa-github',
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+  };
+
   render() {
     return (
       <nav className='navbar bg-primary'>
         <h1>
-          <i className={this.props.icon} style={{ marginRight: 5 }} />
+          <i className={this.props.icon} style={{ marginRight: 6 }} />
           {this.props.title}
         </h1>
       </nav>
