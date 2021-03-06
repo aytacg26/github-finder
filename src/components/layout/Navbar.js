422e98b7011e5815e57ffabb3661f9990f1f3933
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-06 14:46:49 +0200

    Links added to Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const Navbar = ({ title, icon }) => {
@@ -8,6 +9,14 @@ const Navbar = ({ title, icon }) => {
         <i className={icon} style={{ marginRight: 6 }} />
         {title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
