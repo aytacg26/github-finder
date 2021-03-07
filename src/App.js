f2b3d3c2ef4fb7d2c5b3a6fd974d2ad532b17b7e
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-07 21:10:40 +0200

    Home Page and NotFound added to App Comp.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
+import Home from './components/pages/Home';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 import AlertState from './context/alert/AlertState';
 import GitHubState from './context/github/GitHubState';
 import './App.css';
@@ -20,18 +19,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
--- src/context/github/GitHubState.js
+++ src/context/github/GitHubState.js
@@ -10,6 +10,17 @@ import {
   CLEAR_USERS,
 } from '../types';
 
+let githubClientId;
+let githubClientKey;
+
+if (process.env.NODE_ENV !== 'production') {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientKey = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientKey = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GitHubState = (props) => {
   const initialState = {
     users: [],
@@ -25,7 +36,7 @@ const GitHubState = (props) => {
     setLoading();
 
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientKey}`
     );
 
     dispatch({ type: SEARCH_USERS, payload: res.data.items });
@@ -36,7 +47,7 @@ const GitHubState = (props) => {
     setLoading();
 
     const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientKey}`
     );
 
     dispatch({ type: GET_USER, payload: res.data });
@@ -46,7 +57,7 @@ const GitHubState = (props) => {
   const getUserRepos = async (username) => {
     setLoading();
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientKey}`
     );
 
     dispatch({ type: GET_REPOS, payload: res.data });
